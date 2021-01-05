6f5b276cc48d019f787fe2ddced4d0798f4a7293
soupor <546742823@qq,com>
2021-01-05 17:39:11 +0800

    基础文件

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    content: 'hello world'
+  }
+})
+const getMessage = () => "Hello World"
+document.getElementById('output').innerHTML = getMessage()
+
+console.log(d3)
